fdaf51de0eaef8e1a85b0589dd04b4e71dfa5a65
Owner <noa16.he@gmail.com>
2022-01-18 02:11:54 +0200

    dk

--- EcoCareApp/EcoCareApp/App.xaml.cs
+++ EcoCareApp/EcoCareApp/App.xaml.cs
@@ -36,7 +36,7 @@ namespace EcoCareApp
             EcoCareAPIProxy proxy = EcoCareAPIProxy.CreateProxy();
            
             CountriesList = await proxy.GetCountriesAsync();
-            Page p = new Views.Tryouts();
+            Page p = new Views.StartPage();
            
             //p.Title = "Start Page";
             MainPage = new NavigationPage(p) { BarBackgroundColor = Color.FromHex("#81cfe0") };
