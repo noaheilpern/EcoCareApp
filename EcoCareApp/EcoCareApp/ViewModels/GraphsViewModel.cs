a8ff43fc9d7fdb195328f4f9f9e960e8aec36ca3
User <User@LAB3-6>
2022-01-23 10:31:44 +0200

    23/1

---
+++ EcoCareApp/EcoCareApp/ViewModels/GraphsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microcharts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcoCareApp.ViewModels
+{
+    class GraphsViewModel
+    {
+        //public Chart ExampleChart
+        //{
+        //    get
+        //    {
+        //        var f = new List<float>();
+        //        f.Add(100);
+        //        this.ExampleChart.Entries = f; 
+        //    }
+        //    set
+        //    {
+
+        //    }
+        //}
+        //public Chart 
+        //{
+        //    Entries = entries,
+        //    PointMode = PointMode.Circle,
+        //    PointSize = 20,
+        //    BackgroundColor = SKColors.Transparent,
+                 
+               
+            
+        //}
+
+    }
+}
