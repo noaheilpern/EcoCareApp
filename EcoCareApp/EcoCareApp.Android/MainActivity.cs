c60a608f714eccc6e08c4f146404ab7a9f852e02
Owner <noa16.he@gmail.com>
2022-02-06 23:59:27 +0200

    lsiru

--- EcoCareApp/EcoCareApp.Android/MainActivity.cs
+++ EcoCareApp/EcoCareApp.Android/MainActivity.cs
@@ -14,7 +14,6 @@ namespace EcoCareApp.Droid
         {
             base.OnCreate(savedInstanceState);
 
-            Rg.Plugins.Popup.Popup.Init(this);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
